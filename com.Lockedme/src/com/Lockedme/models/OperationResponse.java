7523b73e4656d0ccb018d5febf7065403c9a383d
Priya Diwate-Chaskar <priya36d@gmail.com>
2022-12-08 16:31:27 +0530

    Added a model file.

---
+++ com.Lockedme/src/com/Lockedme/models/OperationResponse.java
@@ -0,0 +1,19 @@
+package com.Lockedme.models;
+
+public class OperationResponse {
+	private boolean isSuccess;
+	private String message;
+	
+	public boolean isSuccess() {
+		return isSuccess;
+	}
+	public void setSuccess(boolean isSuccess) {
+		this.isSuccess = isSuccess;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
